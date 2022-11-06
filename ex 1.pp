4951c00690a2e43ab6b2c2d9b73f67a0d44a6740
markwen0329 <114090782+markwen0329@users.noreply.github.com>
2022-11-06 11:00:09 +0800

    Create ex 1.pp

---
+++ ex 1.pp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, product;
+    a = 20;
+    b = 7;
+
+ 
+    // Calculating product
+    product = a * b;
+    a = a + 3;
+ 
+    printf("Product = %d\n", product);
+    printf("a = %d\n", a);
+    return 0;
+}
